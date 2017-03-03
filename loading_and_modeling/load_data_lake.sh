56fa56f89ce873aadb2f683027845ce97165a5bf
qzhang25 <zhangqing198573@gmail.com>
2017-03-03 14:57:33 -0800

    Create load_data_lake.sh

---
+++ loading_and_modeling/load_data_lake.sh
@@ -0,0 +1,36 @@
+	#Downloading the file 
+
+wget -O hosp.zip https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
+	
+  
+	#Unzipping the files
+	unzip hosp.zip
+
+
+	#Renaming the files 
+	tail -n+2 "Hospital General Information.csv" > "hospitals.csv"
+	tail -n+2 "Timely and Effective Care - Hospital.csv" > "effective_care.csv"
+	tail -n+2 "Readmissions and Deaths - Hospital.csv" > "readmissions.csv"
+	tail -n+2 "Measure Dates.csv" > "Measures.csv"
+	tail -n+2 "hvbp_hcahps_05_28_2015.csv" > "surveys_responses.csv"
+	
+
+	#Adding a file in the w205 folder named hospital compare
+	hdfs dfs -mkdir /user/w205/hospital_compare
+	
+
+	#Adding a folder to store the data above
+	hdfs dfs -mkdir /user/w205/hospital_compare/hosp_gen
+	hdfs dfs -mkdir /user/w205/hospital_compare/time_effec
+	hdfs dfs -mkdir /user/w205/hospital_compare/read_deaths
+	hdfs dfs -mkdir /user/w205/hospital_compare/meas_dates
+	hdfs dfs -mkdir /user/w205/hospital_compare/surv_resp
+	
+
+	#Loading the renamed files above
+	hdfs dfs -put hospitals.csv /user/w205/hospital_compare/hosp_gen
+	hdfs dfs -put effective_care.csv /user/w205/hospital_compare/time_effec
+	hdfs dfs -put readmissions.csv /user/w205/hospital_compare/read_deaths
+	hdfs dfs -put Measures.csv /user/w205/hospital_compare/meas_dates
+	hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare/surv_resp
+
