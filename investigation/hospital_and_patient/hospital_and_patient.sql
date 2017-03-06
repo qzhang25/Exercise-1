f3aaed3b1be338760b04d10b1fa619ba73e58042
qzhang25 <zhangqing198573@gmail.com>
2017-03-06 00:38:02 -0800

    Update hospital_and_patient.sql

--- investigation/hospital_and_patient/hospital_and_patient.sql
+++ investigation/hospital_and_patient/hospital_and_patient.sql
@@ -34,16 +34,16 @@ select  inline
 (
 array
 (
-struct ('Correlation_nur', ( Avg(final_avg * nur_dimession_score) - Avg(final_avg) * Avg(nur_dimession_score)) / (Stddev(final_avg) * Stddev(nur_dimession_score))  )
-,struct ('Correlation_doc', (Avg(final_avg * doc_dimession_score) - Avg(final_avg) * Avg(doc_dimession_score)) / (Stddev(final_avg) * Stddev(doc_dimession_score))  )
+struct ('Correlation_nurse', ( Avg(final_avg * nur_dimession_score) - Avg(final_avg) * Avg(nur_dimession_score)) / (Stddev(final_avg) * Stddev(nur_dimession_score))  )
+,struct ('Correlation_doctor', (Avg(final_avg * doc_dimession_score) - Avg(final_avg) * Avg(doc_dimession_score)) / (Stddev(final_avg) * Stddev(doc_dimession_score))  )
 ,struct ('Correlation_staff', (Avg(final_avg * staff_dimession_score) - Avg(final_avg) * Avg(staff_dimession_score)) / (Stddev(final_avg) * Stddev(staff_dimession_score)) )
 ,struct ('Corrrelation_pain', (Avg(final_avg * pain_dimession_score) - Avg(final_avg) * Avg(pain_dimession_score)) / (Stddev(final_avg) * Stddev(pain_dimession_score))   )
-,struct ('Correlation_med', (Avg(final_avg * med_dimession_score) - Avg(final_avg) * Avg(med_dimession_score)) / (Stddev(final_avg) * Stddev(med_dimession_score))   )
+,struct ('Correlation_medicen', (Avg(final_avg * med_dimession_score) - Avg(final_avg) * Avg(med_dimession_score)) / (Stddev(final_avg) * Stddev(med_dimession_score))   )
 ,struct ('Correlation_clquiet', (Avg(final_avg * clquiet_dimession_score) - Avg(final_avg) * Avg(clquiet_dimession_score)) / (Stddev(final_avg) * Stddev(clquiet_dimession_score)) )
-,struct ('Correlation_disch', (Avg(final_avg * disch_dimession_score) - Avg(final_avg) * Avg(disch_dimession_score)) / (Stddev(final_avg) * Stddev(disch_dimession_score)) )
-,struct ('Correlation_tot_ach', (Avg(final_avg * total_achievement_score) - Avg(final_avg) * Avg(total_achievement_score)) / (Stddev(final_avg) * Stddev(total_achievement_score)) )
-,struct ('Correlation_tot_imp', (Avg(final_avg * total_improvement_score) - Avg(final_avg) * Avg(total_improvement_score)) / (Stddev(final_avg) * Stddev(total_improvement_score)) )
-,struct ('Correlation_tot_dim', (Avg(final_avg * total_dimession_score) - Avg(final_avg) * Avg(total_dimession_score)) / (Stddev(final_avg) * Stddev(total_dimession_score)) )
+,struct ('Correlation_discharge', (Avg(final_avg * disch_dimession_score) - Avg(final_avg) * Avg(disch_dimession_score)) / (Stddev(final_avg) * Stddev(disch_dimession_score)) )
+,struct ('Correlation_total_achievement', (Avg(final_avg * total_achievement_score) - Avg(final_avg) * Avg(total_achievement_score)) / (Stddev(final_avg) * Stddev(total_achievement_score)) )
+,struct ('Correlation_total_improvement', (Avg(final_avg * total_improvement_score) - Avg(final_avg) * Avg(total_improvement_score)) / (Stddev(final_avg) * Stddev(total_improvement_score)) )
+,struct ('Correlation_total_dimession', (Avg(final_avg * total_dimession_score) - Avg(final_avg) * Avg(total_dimession_score)) / (Stddev(final_avg) * Stddev(total_dimession_score)) )
 )
 ) as (Surv_Resp,Correlation)
 from    COMBINE_HOSPITAL;
