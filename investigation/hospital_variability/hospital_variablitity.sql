260ffced60213d4580485026f4e5587f5147b847
qzhang25 <zhangqing198573@gmail.com>
2017-03-05 23:43:06 -0800

    Update hospital_variablitity.sql

--- investigation/hospital_variability/hospital_variablitity.sql
+++ investigation/hospital_variability/hospital_variablitity.sql
@@ -1,22 +1,22 @@
 --standard deviation table.
 
  
-
+DROP TABLE STDEVIATION_FT;
 create table STDEVIATION_FT
 as
 select  inline
         (
             array
             (
-                struct ('EMR'   ,STDDEV (emr_avg_score)  )
-               ,struct ('SCI'   ,STDDEV (sci_avg_score)  )
-               ,struct ('ASTH'  ,STDDEV (asth_avg_score) )
-               ,struct ('HF'    ,STDDEV (hf_avg_score)   )
-               ,struct ('SC'    ,STDDEV(sc_avg_score)   )
+                struct ('EMR'   ,STDDEV (emergency_avg_score)  )
+               ,struct ('SCI'   ,STDDEV (scarei_avg_score)  )
+               ,struct ('ASTH'  ,STDDEV (childasth_avg_score) )
+               ,struct ('HF'    ,STDDEV (hospitalf_avg_score)   )
+               ,struct ('SC'    ,STDDEV(strokec_avg_score)   )
 
             )
         ) as (HOSP_VAR,RANGE)
  
 from    COMBINE4;    
-
+--Print the result
 SELECT  * FROM STDEVIATION_FT;
