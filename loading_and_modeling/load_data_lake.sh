843d25fbeea614e6f4689d8ad1dd1d78c5e4fd77
qzhang25 <zhangqing198573@gmail.com>
2017-03-07 17:17:17 -0800

    Update load_data_lake.sh

--- loading_and_modeling/load_data_lake.sh
+++ loading_and_modeling/load_data_lake.sh
@@ -1,4 +1,4 @@
-	#Download file
+	#Download file from the website
 wget -O hosp.zip https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
 	
   
